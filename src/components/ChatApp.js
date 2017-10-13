b5d76ae5d441c065f8c092c4503f5956d73dc906
Ben Askins <ben.askins@envato.com>
2017-10-13 13:33:09 +1100

    Map out the chat UI

--- src/components/ChatApp.js
+++ src/components/ChatApp.js
@@ -1,7 +1,32 @@
 import React from 'react'
 
+const MessageEntry = 'div'
+const Avatar = 'div'
+const MessageText = 'div'
+const Screen = 'div'
+const ChatWindow = 'div'
+const ChatInput = 'div'
+
+const Message = () => (
+  <MessageEntry>
+    <Avatar/>
+    <MessageText>
+      <div>Line one</div>
+      <div>Line two</div>
+      <div>Line three</div>
+    </MessageText>
+  </MessageEntry>
+)
+
 const ChatApp = () => (
-  <h1>Chat with me</h1>
+  <Screen>
+    <ChatWindow>
+      <Message></Message>
+      <Message></Message>
+      <Message></Message>
+    </ChatWindow>
+    <ChatInput/>
+  </Screen>
 )
 
 export default ChatApp
