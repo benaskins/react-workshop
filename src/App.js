9b997be5fdc35491916a85430322abadb1598227
Ben Askins <ben.askins@envato.com>
2017-10-13 11:11:23 +1100

    Handle a button click

--- src/App.js
+++ src/App.js
@@ -10,12 +10,21 @@ const AppHeader = ({ greeting }) => (
 )
 
 class App extends Component {
+  handleButtonClick() {
+    console.log("handle click")
+  }
+
   render() {
     return (
       <div className="App">
         <AppHeader greeting="Helloooooooo"/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          <button type="buttons" onClick={this.handleButtonClick}>
+            Click me!
+          </button>
         </p>
       </div>
     )
