ed50b230c1eae059d66c1458080d1dbcf647eb84
Ben Askins <ben.askins@envato.com>
2017-10-13 13:28:36 +1100

    Start on the chat app

---
+++ src/components/ChatApp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ChatApp = () => (
+  <h1>Chat with me</h1>
+)
+
+export default ChatApp
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import ChatApp from './components/ChatApp'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<ChatApp />, document.getElementById('root'))
