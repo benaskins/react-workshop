ddb10bb245b7cf9ae433c2b6dcb1bdc67d7f8050
Ben Askins <ben.askins@envato.com>
2017-10-13 11:41:16 +1100

    Factor out greeting changer and appintro

--- src/App.js
+++ src/App.js
@@ -9,6 +9,21 @@ const AppHeader = ({ greeting }) => (
   </header>
 )
 
+const GreetingChanger = ({ target }) => (
+  <p>
+    Change the greeting...
+    <button type="buttons" onClick={target.handleButtonClick}>
+      Click me!
+    </button>
+  </p>
+)
+
+const AppIntro = () => (
+  <p className="App-intro">
+    To get started, edit <code>src/App.js</code> and save to reload.
+  </p>
+)
+
 class App extends Component {
   state = { greeting: "Howdy!!!" }
 
@@ -24,14 +39,8 @@ class App extends Component {
     return (
       <div className="App">
         <AppHeader greeting={ this.state.greeting }/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <button type="buttons" onClick={this.handleButtonClick}>
-            Click me!
-          </button>
-        </p>
+        <AppIntro/>
+        <GreetingChanger target={this}/>
       </div>
     )
   }
