2faf70cacaadb8d4cb5ccedea90d118db88e6dad
Ben Askins <ben.askins@envato.com>
2017-10-13 11:08:04 +1100

    Allow greeting to be passed to header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-const AppHeader = () => (
+const AppHeader = ({ greeting }) => (
   <header className="App-header">
     <img src={logo} className="App-logo" alt="logo" />
-    <h1 className="App-title">Welcome to React</h1>
+    <h1 className="App-title">{ greeting }</h1>
   </header>
 )
 
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <AppHeader/>
+        <AppHeader greeting="Helloooooooo"/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
